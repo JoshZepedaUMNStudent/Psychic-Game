209d8a9862f3306d601133c58d8d88aefce8b7b3
JZStudent <joshzepeda.student@gmail.com>
2018-05-25 21:07:44 -0500

    updated May 25

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+
+// Array
+var letters = ['a,','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+              'q','r','s','t','u','v','w','x','y','z'];
+
+// Variables
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+
+var compAnswer = letters[Math.floor(Math.random() * letters.length)];
+    console.log(compAnswer);
+   
+function renderPage(){
+    document.getElementById("win").innerHTML = wins;
+    document.getElementById("loss").innerHTML = losses;
+    document.getElementById("remain").innerHTML = guesses;
+}
+
+function reset(){
+    guesses = 10;
+}
+document.onkeypress = function(event) {
+    var playerGuess = event.key;
+
+    if(playerGuess === compAnswer) {
+        wins++;
+        reset() 
+        alert("You win!");
+    }
+    else {
+        guesses--;
+    }
+    if(guesses === 0) {
+        losses++;
+        guesses = 10;
+
+        alert("You lose!");
+    }
+
+
+    renderPage()
+
+   
+
+}
