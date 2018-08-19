4bffeef676ba01cffa8ab2acda09d30eebd3b0c8
JZStudent <joshzepeda.student@gmail.com>
2018-08-19 09:46:20 -0500

    3 of 4 tests successful

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,23 +3,28 @@
 var letters = ['a,','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
               'q','r','s','t','u','v','w','x','y','z'];
 
-// Variables
+// Basic Variables
 var wins = 0;
 var losses = 0;
 var guesses = 10;
 
+// Letter Generator
 var compAnswer = letters[Math.floor(Math.random() * letters.length)];
     console.log(compAnswer);
-   
+
+// Page Content
 function renderPage(){
     document.getElementById("win").innerHTML = wins;
     document.getElementById("loss").innerHTML = losses;
     document.getElementById("remain").innerHTML = guesses;
 }
 
+// Game Reset
 function reset(){
     guesses = 10;
 }
+
+// Game Functionality
 document.onkeypress = function(event) {
     var playerGuess = event.key;
 
